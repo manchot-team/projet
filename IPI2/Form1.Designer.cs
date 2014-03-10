6d103da661d82caacd18b79bcc4b1645d3000910
abrantes01 <abrantes.kevin@gmail.com>
2014-03-10 12:09:10 +0100

    Centrage bouton

--- IPI2/Form1.Designer.cs
+++ IPI2/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(83, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(111, 23);
             this.button1.TabIndex = 0;
