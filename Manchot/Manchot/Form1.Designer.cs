775cf61b9ad36a3ca7178b248c995419f5250f0e
abrantes01 <abrantes.kevin@gmail.com>
2014-05-06 09:30:46 +0200

    ADD Debut analyse evenement, Somme des courbes

--- Manchot/Manchot/Form1.Designer.cs
+++ Manchot/Manchot/Form1.Designer.cs
@@ -32,16 +32,25 @@
             this.waveformPlot1 = new NationalInstruments.UI.WaveformPlot();
             this.xAxis1 = new NationalInstruments.UI.XAxis();
             this.yAxis1 = new NationalInstruments.UI.YAxis();
+            this.waveformPlot2 = new NationalInstruments.UI.WaveformPlot();
             this.btn_open_file = new System.Windows.Forms.Button();
+            this.waveformPlot3 = new NationalInstruments.UI.WaveformPlot();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.waveformPlot4 = new NationalInstruments.UI.WaveformPlot();
             ((System.ComponentModel.ISupportInitialize)(this.waveformGraph1)).BeginInit();
             this.SuspendLayout();
             // 
             // waveformGraph1
             // 
-            this.waveformGraph1.Location = new System.Drawing.Point(12, 12);
+            this.waveformGraph1.Location = new System.Drawing.Point(12, 43);
             this.waveformGraph1.Name = "waveformGraph1";
             this.waveformGraph1.Plots.AddRange(new NationalInstruments.UI.WaveformPlot[] {
-            this.waveformPlot1});
+            this.waveformPlot1,
+            this.waveformPlot2,
+            this.waveformPlot3,
+            this.waveformPlot4});
             this.waveformGraph1.Size = new System.Drawing.Size(833, 336);
             this.waveformGraph1.TabIndex = 0;
             this.waveformGraph1.UseColorGenerator = true;
@@ -55,9 +64,14 @@
             this.waveformPlot1.XAxis = this.xAxis1;
             this.waveformPlot1.YAxis = this.yAxis1;
             // 
+            // waveformPlot2
+            // 
+            this.waveformPlot2.XAxis = this.xAxis1;
+            this.waveformPlot2.YAxis = this.yAxis1;
+            // 
             // btn_open_file
             // 
-            this.btn_open_file.Location = new System.Drawing.Point(12, 354);
+            this.btn_open_file.Location = new System.Drawing.Point(12, 385);
             this.btn_open_file.Name = "btn_open_file";
             this.btn_open_file.Size = new System.Drawing.Size(75, 23);
             this.btn_open_file.TabIndex = 2;
@@ -65,11 +79,53 @@
             this.btn_open_file.UseVisualStyleBackColor = true;
             this.btn_open_file.Click += new System.EventHandler(this.btn_open_file_Click);
             // 
+            // waveformPlot3
+            // 
+            this.waveformPlot3.XAxis = this.xAxis1;
+            this.waveformPlot3.YAxis = this.yAxis1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Analyser";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(867, 43);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(173, 329);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(184, 386);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // waveformPlot4
+            // 
+            this.waveformPlot4.XAxis = this.xAxis1;
+            this.waveformPlot4.YAxis = this.yAxis1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(857, 389);
+            this.ClientSize = new System.Drawing.Size(1052, 417);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_open_file);
             this.Controls.Add(this.waveformGraph1);
             this.Name = "Form1";
@@ -86,6 +142,12 @@
         private NationalInstruments.UI.XAxis xAxis1;
         private NationalInstruments.UI.YAxis yAxis1;
         private System.Windows.Forms.Button btn_open_file;
+        private NationalInstruments.UI.WaveformPlot waveformPlot2;
+        private NationalInstruments.UI.WaveformPlot waveformPlot3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private NationalInstruments.UI.WaveformPlot waveformPlot4;
     }
 }
 
--- Manchot/Manchot/Form1.cs
+++ Manchot/Manchot/Form1.cs
@@ -32,10 +32,11 @@ namespace Manchot
             InitializeComponent();
         }
 
+        double[][] measuredData = new double[3][];
 
         private void btn_open_file_Click(object sender, EventArgs e)
         {
-            String[] input = new String[10];
+            String[] input = new String[3];
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.Filter ="tdms files (*.tdms)|*.tdms|All files (*.*)|*.*";
             dialog.Multiselect = true;
@@ -51,8 +52,8 @@ namespace Manchot
             }*/
             //Console.WriteLine("Chemin fichier : " + input);
             long numChanValues;
-            double[] measuredData;
-            TdmsFile[] files = new TdmsFile[10];
+            
+            TdmsFile[] files = new TdmsFile[3];
             TdmsChannelGroupCollection channelGroups;
             TdmsChannelCollection channels;
 
@@ -61,30 +62,32 @@ namespace Manchot
                 files[i] = new TdmsFile(input[i], TdmsFileAccess.Read);
                 Console.WriteLine("FILE["+i+"] = "+files[i].Path+"\n");
             }
-
+            Console.WriteLine(files.Length);
+            for (int i = 0; i < files.Length; i++)
+            {
+                channelGroups = files[i].GetChannelGroups();
+                channels = channelGroups[0].GetChannels();
+                numChanValues = channels[0].DataCount;
+                Console.WriteLine(numChanValues);
+                measuredData[i] = channels[0].GetData<double>();
+                files[i].Close();
+            }
             //Read group data
-            channelGroups = files[0].GetChannelGroups();
-            channels = channelGroups[0].GetChannels();
-            numChanValues = channels[0].DataCount;
 
-            Console.WriteLine(numChanValues);
-            measuredData = channels[0].GetData<double>();
 
-            //Close file
-            files[0].Close();
 
             //Console.WriteLine(numChanValues.ToString());
             double max = 0;
             double data_a_regarder = 0;
             bool trouve = false;
-            for (int i = 0; i < measuredData.Length; i++)
+            for (int i = 0; i < measuredData[1].Length; i++)
             {
-                if (measuredData[i] > max)
+                if (measuredData[1][i] > max)
                 {
-                    max = measuredData[i];
+                    max = measuredData[1][i];
                 }
 
-                if (measuredData[i] >= 2 && !trouve)
+                if (measuredData[1][i] >= 2 && !trouve)
                 {
                     data_a_regarder = i;
                     trouve = true;
@@ -92,11 +95,71 @@ namespace Manchot
                 
             }
 
+            Console.WriteLine(data_a_regarder);
             waveformGraph1.YAxes[0].Mode = AxisMode.Fixed;
             waveformGraph1.YAxes[0].Range = new NationalInstruments.UI.Range(0.0, 12.0);
             waveformGraph1.XAxes[0].Mode = AxisMode.Fixed;
-            waveformGraph1.XAxes[0].Range = new NationalInstruments.UI.Range(data_a_regarder-200, data_a_regarder+200);
-            waveformGraph1.PlotY(measuredData);
+            waveformGraph1.XAxes[0].Range = new NationalInstruments.UI.Range(data_a_regarder - 200, data_a_regarder + 300);
+            waveformPlot1.PlotY(measuredData[0]);
+            waveformPlot2.PlotY(measuredData[1]);
+            waveformPlot3.PlotY(measuredData[2]);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double max = 0;
+            double[] data_a_regarder = new double[30];
+            bool trouve = false;
+            for (int i = 0, j = 0; i < measuredData[1].Length; i++)
+            {
+                if (measuredData[1][i] > max)
+                {
+                    max = measuredData[1][i];
+                }
+
+                if (measuredData[1][i] >= 2 && !trouve)
+                {
+                    data_a_regarder[j] = i;
+                    j++;
+                    trouve = true;
+                }
+
+                if (measuredData[1][i] <= 2 && trouve)
+                {
+                    trouve = false;
+                }
+
+            }
+
+            Console.Write(data_a_regarder);
+
+            foreach ( double index in data_a_regarder ){
+                listBox1.Items.Add(index);
+                Console.WriteLine(index);
+            }
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Element selectionné: " + listBox1.SelectedItem.ToString());
+            double data_a_regarder = Convert.ToDouble(listBox1.SelectedItem.ToString());
+            waveformGraph1.YAxes[0].Mode = AxisMode.Fixed;
+            waveformGraph1.YAxes[0].Range = new NationalInstruments.UI.Range(0.0, 12.0);
+            waveformGraph1.XAxes[0].Mode = AxisMode.Fixed;
+            waveformGraph1.XAxes[0].Range = new NationalInstruments.UI.Range(data_a_regarder - 200, data_a_regarder + 300);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double[] measuredDataFusionned = new double[measuredData[1].Length];
+
+            for (int i = 0; i < measuredData[1].Length; i++)
+            {
+                measuredDataFusionned[i] = measuredData[0][i] + measuredData[1][i] + measuredData[2][i];
+            }
+            waveformPlot4.PlotY(measuredDataFusionned);
 
         }
     }
