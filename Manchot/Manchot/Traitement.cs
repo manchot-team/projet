9b1e7ef18f8465a1462c8ce38f2123b27a675465
shARPIII <philippe.grad@gmail.com>
2014-05-09 16:13:30 +0200

    conflit

---
+++ Manchot/Manchot/Traitement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manchot
+{
+    class Traitement
+    {
+        public static void supressionBruit()
+        {
+            //Console.WriteLine("Salut");
+        }
+    }
+}
